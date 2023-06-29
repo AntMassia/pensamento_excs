709519baa593faa629a19b710251ef630e2b2dc6
AntMassia <126731472+AntMassia@users.noreply.github.com>
2023-06-29 16:20:02 -0300

    Update exc10.py

--- exercicios/exc10.py
+++ exercicios/exc10.py
@@ -1,1 +1,9 @@
+def soma(tupla):
+    prod = 1
+    for elemento in tupla:
+        prod *= elemento
+    return produto
 
+tupla = (9, -3, 12, 6, 34, -17)
+prod = soma(tupla)
+print("O Resultado da tupla", tupla, "é Igual:", prod)
