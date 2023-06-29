2dd395198cdab7e1ab31b08a6c61226ea877902a
AntMassia <126731472+AntMassia@users.noreply.github.com>
2023-06-29 15:09:20 -0300

    Update exc02.py

--- exercicios/exc02.py
+++ exercicios/exc02.py
@@ -1,1 +1,19 @@
+def bissexto(ano):
+    if ano % 4 == 0:
+        if ano % 100 == 0:
+            if ano % 400 == 0:
+                return True
+            else:
+                return False
+            
+        else:
+            print("O Ano é Bissexto")
+            return True
+    else:
+        print("O Ano não é Bissexto")
+        return False
+        
+ano = 2032
+resultado = bissexto(ano)
+print(resultado) 
 
