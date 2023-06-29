905246792004981bab49c92aeaa531613f467567
AntMassia <126731472+AntMassia@users.noreply.github.com>
2023-06-29 16:28:39 -0300

    Update exc12.py

--- exercicios/exc12.py
+++ exercicios/exc12.py
@@ -1,1 +1,10 @@
 
+def roger(string):
+    palavras = string.split('-')
+    ordenadas = sorted(palavras)
+    strings = '-'.join(ordenadas)
+    return strings
+
+char = "xbox-playstation-nintendo-atari"
+resultado = roger(char)
+print(resultado)
