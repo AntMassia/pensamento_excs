3150000d178aa97bc5ffebfaeb04b19b211de60c
AntMassia <126731472+AntMassia@users.noreply.github.com>
2023-06-29 14:59:45 -0300

    Create exc 01.py

---
+++ exc 01.py
@@ -0,0 +1,11 @@
+def verifica_soma(a, b, limite):
+    soma = a + b
+    if soma > limite:
+        return True
+    else:
+        return False
+
+resultado = verifica_soma(8,2,7)
+print(resultado)
+
+    
