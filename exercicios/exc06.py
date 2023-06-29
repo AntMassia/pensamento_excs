0c8e0b430ebf3bbe398e802f10838b74407f29f8
AntMassia <126731472+AntMassia@users.noreply.github.com>
2023-06-29 15:46:17 -0300

    Update exc06.py

--- exercicios/exc06.py
+++ exercicios/exc06.py
@@ -1,1 +1,11 @@
+def strings(lista, n):
+    resultado = []
+    for palavra in lista:
+        if len(palavra) > n:
+            resultado.append(palavra)
+    return resultado
 
+lista = [ "xbox", "playstation", "nintendo", "atari"]
+n = 5
+stringsmaiores = strings(lista, n)
+print(stringsmaiores)
