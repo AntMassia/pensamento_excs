3d08631ea97523a7de7226e6327a65dbce454708
AntMassia <126731472+AntMassia@users.noreply.github.com>
2023-06-29 15:29:11 -0300

    Update exc04.py

--- exercicios/exc04.py
+++ exercicios/exc04.py
@@ -1,1 +1,9 @@
-
+def contagem(numero):
+    if numero == 0:
+        print("Decolar!")
+    else:
+        print(numero)
+        contagem(numero - 1)
+        
+num = int(input("Digite um número para iniciar a contagem regressiva: "))
+contagem(num)
