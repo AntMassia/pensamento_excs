c9f62f3552dae2055f5232bd7d5f5c400c509d7a
AntMassia <126731472+AntMassia@users.noreply.github.com>
2023-06-29 16:33:43 -0300

    Update exc13.py

--- exercicios/exc13.py
+++ exercicios/exc13.py
@@ -1,1 +1,19 @@
 
+def roger(lista):
+    if type(lista) != list:
+        raise ValueError("O argumento deve ser uma lista")
+
+    elementos = []
+    for elemento in lista:
+        if lista.count(elemento) == 1:
+            elementos.append(elemento)
+
+    return elementos
+
+try:
+    numeros = input("Digite uma lista de números separados por espaço: ").split()
+    numeros = [int(num) for num in numeros]
+    resultado = roger(numeros)
+    print("Elementos únicos da lista:", resultado)
+except ValueError as error:
+    print(error)
