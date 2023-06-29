e0451d4b6fec61df824de9354ed733c8caf570e8
AntMassia <126731472+AntMassia@users.noreply.github.com>
2023-06-29 16:10:07 -0300

    Update exc08.py

--- exercicios/exc08.py
+++ exercicios/exc08.py
@@ -1,1 +1,16 @@
 
+def rogerio(lista):
+    tamanho = len(lista)
+    for i in range(tamanho - 1):
+        trocas = False
+        for n in range(tamanho - 1 - i):
+            if lista[n][1] > lista[n + 1][1]:
+                lista[n], lista[n + 1] = lista[n + 1], lista[n]
+                trocas = True
+        if not trocas:
+            break
+    return lista
+
+lista = [(5, 7), (9, 2), (7, 2), (5, 4), (6, 3)]
+ceni = rogerio(lista)
+print(ceni)
