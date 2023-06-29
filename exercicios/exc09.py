2a2536e7caf94106a931c4b09f5d745591c9b2a1
AntMassia <126731472+AntMassia@users.noreply.github.com>
2023-06-29 16:16:58 -0300

    Update exc09.py

--- exercicios/exc09.py
+++ exercicios/exc09.py
@@ -1,1 +1,11 @@
+import math
 
+def raizz(n):
+    if n < 0:
+        print("Inválido!!!")
+    else: 
+        raiz = math.sqrt(n)
+        print("A Raiz Quadrada de", n, "é Igual a:", raiz)
+
+num = float(input("Digite um Número para Saber a sua Raiz: "))
+raizz(num)
