70f5bd6bee6208290c9ad9146943ea4a8b00066f
AntMassia <126731472+AntMassia@users.noreply.github.com>
2023-06-29 16:39:13 -0300

    Update exc14.py

--- exercicios/exc14.py
+++ exercicios/exc14.py
@@ -1,1 +1,20 @@
 
+def extrair_elemento(lista, n):
+    elementos = []
+    for tupla in lista:
+        if len(tupla) > n:
+            elementos.append(tupla[n])
+        else:
+            raise IndexError("Índice fora dos limites para pelo menos uma das tuplas")
+
+    return elementos
+
+try:
+    lista_tuplas = [("Émily", 14), ("Jéssica", 19), ("Gabi", 24)]
+    n = int(input("Digite o índice para extrair o elemento: "))
+    resultado = extrair_elemento(lista_tuplas, n)
+    print("Elementos extraídos:", resultado)
+except ValueError:
+    print("Entrada inválida. Digite um número inteiro.")
+except IndexError as error:
+    print(error)
