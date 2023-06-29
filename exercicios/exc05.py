a21515e25d8f149d94d5ce9e2b9bf3d86bab392f
AntMassia <126731472+AntMassia@users.noreply.github.com>
2023-06-29 15:50:08 -0300

    Update exc05.py

--- exercicios/exc05.py
+++ exercicios/exc05.py
@@ -1,1 +1,11 @@
 
+def inverte(lista):
+    if len(lista) <= 1:
+        return lista
+    else:
+        return [lista[-1]] + inverte(lista[:-1])
+        
+listt = [1, 2, 3, 4, 5]
+reverse = inverte(listt)
+print(reverse) 
+
